8942ce5b65519cb1ee35473049dff8430c8842d1
Denesz009 <peterfi.denes.24f@szlgbp.hu>
2025-10-08 21:14:36 +0200

    Add files via upload

---
+++ orgonasip/orgonasip/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orgonasip
+{
+    internal class Program
+    {
+        static Random r = new Random(); // general random szamokat
+
+        static void Orgonasipok(List<int> lista)
+        {
+            lista.Sort(); 
+            lista.Reverse(); 
+
+            List<int> lista2 = new List<int>();
+            lista2.Add(lista[0]); //leteszi a legnagyobb szamot
+
+            bool jobbra = true; // valtakozva tesszük be az elemeket
+            for (int i = 1; i < lista.Count; i++)
+            {
+                if (jobbra) 
+                {
+                    lista2.Add(lista[i]); //lista vegere
+                }
+                else 
+                {
+                    lista2.Insert(0, lista[i]); // lista elejere
+                }
+                jobbra = !jobbra; // iranvaltas
+            }
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                lista[i] = lista2[i];
+            }
+        }
+
+        static List<int> Generator(int hossz, int legkisebb, int legnagyobb)
+        {
+            List<int> lista = new List<int>();
+            for (int i = 0; i < hossz; i++)
+            {
+                lista.Add(r.Next(legkisebb, legnagyobb + 1)); // mindig uj szám, mivel az r maradt
+            }
+            return lista;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teszt:\n");
+
+            for (int i = 1; i <= 3; i++) // teszteles
+            {
+                List<int> szamok = Generator(10, 1, 67);
+
+                Console.WriteLine($"Alap {i}:");
+                Console.WriteLine(string.Join(", ", szamok));
+
+                Orgonasipok(szamok);
+
+                Console.WriteLine("Rendezett:");
+                Console.WriteLine(string.Join(", ", szamok));
+                Console.WriteLine(new string('-', 50));
+            }
+        }
+    }
+}
---
+++ orgonasip/orgonasip/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("orgonasip")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("orgonasip")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7dc69d27-7dee-4f32-aec4-253197d7934a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
