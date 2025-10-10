22bb74f8302eb221a7c43e9fb4c7d665a5e8f2f3
Denesz009 <peterfi.denes.24f@szlgbp.hu>
2025-10-10 19:33:47 +0200

    Add files via upload

---
+++ advent_of_code_1(rendezesek)/advent_of_code_1(rendezesek)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent_of_code_1_rendezesek_
+{
+    internal class Program
+    {
+        
+        static int TeljesTavolsag(List<int> ballista, List<int> jobblista)
+        {
+            int tavolsag = 0;
+            for (int i = 0; i < ballista.Count; i++)
+            {
+                int kulonbseg = Math.Abs(ballista[i] - jobblista[i]);
+                Console.WriteLine($"{i + 1}. pár: {ballista[i]} és {jobblista[i]} ->" +
+                    $" " + $"távolság: {kulonbseg}");
+                tavolsag += kulonbseg;
+            }
+            Console.WriteLine($"\nteljes távolság: {tavolsag}");
+            return tavolsag;
+        }
+        static void Main(string[] args)
+        {
+            List<int> ballista = new List<int> { 3, 4, 2, 1, 3, 3 };
+            List<int> jobblista = new List<int> { 4, 3, 5, 3, 9, 3 };
+            TeljesTavolsag(ballista, jobblista);
+        }
+    }
+}
