026f6dcab5ccb8e087dc708283847de608a50d85
Denesz009 <peterfi.denes.24f@szlgbp.hu>
2025-10-20 20:23:14 +0200

    Add files via upload

---
+++ pascal/pascal/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal
+{
+    internal class Program
+    {
+        static string[,] Pascal_teglalap()
+        {
+            return new string[,]
+            {
+            { "P", "A", "S", "C" },
+            { "A", "S", "C", "A" },
+            { "S", "C", "A", "L" }
+            };
+        }
+        static void Teglalap_kiiras(string[,] teglalap)
+        {
+            int sorok = teglalap.GetLength(0);
+            int oszlopok = teglalap.GetLength(1);
+
+            for (int i = 0; i < sorok; i++)
+            {
+                for (int j = 0; j < oszlopok; j++)
+                {
+                    Console.Write(teglalap[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static int SzamolUtak(int sorok, int oszlopok)
+        {
+            int[,] utak = new int[sorok, oszlopok];
+            for (int j = 0; j < oszlopok; j++)
+            {
+                utak[0, j] = 1;
+            }
+            for (int i = 0; i < sorok; i++)
+            {
+                utak[i, 0] = 1;
+            }
+            for (int i = 1; i < sorok; i++)
+            {
+                for (int j = 1; j < oszlopok; j++)
+                {
+                    utak[i, j] = utak[i - 1, j] + utak[i, j - 1];
+                }
+            }
+
+            return utak[sorok - 1, oszlopok - 1];
+        }
+        static void Main(string[] args)
+        {
+            string[,] teglalap = Pascal_teglalap();
+            Teglalap_kiiras(teglalap);
+            int utakSzama = SzamolUtak(teglalap.GetLength(0),
+            teglalap.GetLength(1));
+            Console.WriteLine(utakSzama);
+        }
+    }
+}
---
+++ pascal/pascal/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("pascal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pascal")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45fa6b38-6cee-45d2-9f94-afa162974ce4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
